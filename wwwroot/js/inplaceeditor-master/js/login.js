8e6642d7be45f468ae4d10464d7f29c3b4936414
James <jamesrudd99@gmail.com>
2021-01-23 21:06:26 +0000

    Removed Incorrect Files

--- wwwroot/js/inplaceeditor-master/js/login.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-	var controlsCss = ' style=" bottom: 20px; position: absolute; right: 18px; "';
-        $('body').append('<div class="editorControls" ' + controlsCss + '><a href="/login">Admin</div>');
-});
